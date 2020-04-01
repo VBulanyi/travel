c97262ede09331a7cddaafc267cee5f42b6c153b
Vladimir Bulanyi <jetbul@gmail.com>
2020-04-01 14:09:28 +0300

    webpack added

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable quote-props */
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        edge: '15',
+        firefox: '60',
+        chrome: '64',
+        safari: '11.1',
+      },
+      useBuiltIns: 'usage',
+      corejs: '3.0.0',
+      'targets': {
+        'esmodules': true,
+        'ie': '11',
+      },
+    },
+  ],
+];
+
+module.exports = { presets };
